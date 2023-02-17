d63a596dbfd00c86faa835121e92997765c2744f
KazunguDev <kazungudev@gmail.com>
2023-02-17 12:59:17 +0300

    Init

---
+++ Basic.java
@@ -0,0 +1,10 @@
+public class Basic {
+    public static void main (String [] arg)
+    {
+        // for loop
+        for(int count = 0; count <= 5; count ++)
+        {
+            System.out.println(count);
+        }
+    }
+}
